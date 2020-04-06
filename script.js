95156ad854e92cceadd80b9accf1a8fc8b580477
v-rmz <62731556+v-rmz@users.noreply.github.com>
2020-04-05 21:13:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function changeColor()
 {
-  color = document.getElementById("colorChoice");
+  color = document.getElementById("colorChoice").value;
   if (color == "blue" || color == "red" || color == "yellow"){
     document.body.style.backgroundColor = color;
   }
