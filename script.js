5721de30cd99dbdb37af9c750d69b014708138de
v-rmz <62731556+v-rmz@users.noreply.github.com>
2020-04-05 20:43:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 function changeColor()
 {
   color = document.getElementById("colorChoice").value;
-  if (color == "yellow"){
-    document.body.style.backgroundColor = Yellow;
-  }
-  if (color == "blue"){
-    document.body.style.backgroundColor = Blue;
-
-  }
-
-  if (color == "red"){
-    document.body.style.backgroundColor = Red;
-
-  }
-/*  if (color == "blue" || color == "red" || color == "yellow"){
+  // if (color == "yellow"){
+  //   document.body.style.backgroundColor = Yellow;
+  // }
+  // if (color == "blue"){
+  //   document.body.style.backgroundColor = Blue;
+  // }
+  //
+  // if (color == "red"){
+  //   document.body.style.backgroundColor = Red;
+  // }
+ if (color == "blue" || color == "red" || color == "yellow"){
     document.body.style.backgroundColor = color;
-  }*/
+  }
 }
